fa6f0cda605ecd7424c4812d682d076089290679
mevelec <adrien.mevelec@hotmail.fr>
2019-08-29 17:59:52 +0200

    continue

---
+++ resources/views/components/advantages/advantages.blade.php
@@ -0,0 +1,30 @@
+<div class="advantages">
+    <h2>Le Yoga peut vous aider ! </h2>
+
+    <div class="list">
+        <div class="item">
+            <div class="circle">
+                <img src="{{asset('images/icons/warrior.png')}}">
+            </div>
+            <div class="text">
+                Retrouvez souplesse et dynamisme
+            </div>
+        </div>
+        <div class="item">
+            <div class="circle">
+                <img src="{{asset('images/icons/yin-yang.png')}}">
+            </div>
+            <div class="text">
+                Gérez le stress au quotidien
+            </div>
+        </div>
+        <div class="item">
+            <div class="circle">
+                <img src="{{asset('images/icons/yoga.png')}}">
+            </div>
+            <div class="text">
+                Apaisez les tensions avec des conséquences postives<br> pour soi-même et son entourage
+            </div>
+        </div>
+    </div>
+</div>
---
+++ resources/views/components/lessonsTimes/lessonsTimes.blade.php
@@ -0,0 +1,49 @@
+<div class="lessonsTimes">
+    <div class="title">
+        <h2>Horaires et lieux des cours</h2>
+    </div>
+    <div class="content">
+        <div>
+            <div class="item">
+                <div class="day">
+                    <b>Mardi</b><br>
+                    <span class="times">
+                        10h à 11h30
+                    </span>
+                </div>
+                <div class="place">
+                    <a href="https://goo.gl/maps/XomBDvFkh41c662c7" target="_blank">
+                        Résidence "Bocage parc", 21-27 Rue Croix Montoire, 37100 Tours
+                    </a>
+                    <br>
+                    <span class="nearStop">
+                        Arrêt de tram : 
+                        <a href="https://goo.gl/maps/KyGA37kwLWDyLjg2A" target="_blank">
+                            "Tranchée"
+                        </a>
+                    </span>
+                </div>
+            </div>
+            <div class="item">
+                <div class="day">
+                        <b>Mercredi</b><br>
+                    <span class="times">
+                        18h 45 à 20h15
+                    </span>
+                </div>
+                <div class="place">
+                    <a href="https://goo.gl/maps/b7TohdS4qPHrhE1t5" target="_blank">
+                        Centre Social Pluriel(le)s, 6 avenue du GAL de Gaulle (quartier du Sanitas)
+                    </a>
+                    <br>
+                    <span class="nearStop" >
+                        Arrêt tram: 
+                        <a href="https://goo.gl/maps/D1piFQzaqCUqhbFF7" target="_blank">
+                            "Liberté"
+                        </a>
+                    </span>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
